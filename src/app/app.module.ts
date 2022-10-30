539e5b83f69072efd5eb2492a4e39cfa66a96346
Vedant Rokde <vedant24rokde@gmail.com>
2022-10-30 11:25:53 +0530

    Final commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NotifierModule } from 'angular-notifier';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NotifierModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
